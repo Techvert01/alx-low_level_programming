c35a69bbbab2a688df327e0abcdd01a1b8666155
Tech <techsavvy91@outlook.com>
2023-02-16 18:33:25 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - C that prints various types
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %lu byte(S)\n", (unsigneed long)size of(a));
+	printf("Size of an int: %lu byte(S)\n", (unsigneed long)size of(b));
+	printf("Size of a long int: %lu byte(S)\n", (unsigneed long)size of(c));
+	printf("Size of a long long int: %lu byte(S)\n", (unsigneed long)size of(d));
+	printf("Size of a float: %lu byte(S)\n", (unsigneed long)size of(e));
+	return (0)
+}
