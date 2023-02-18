73b7f26d79771a3585d4cd100fabb924cdf3119b
Tech <techsavvy91@outlook.com>
2023-02-17 16:11:10 -0800

    commiting

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the reverse lowercase alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
