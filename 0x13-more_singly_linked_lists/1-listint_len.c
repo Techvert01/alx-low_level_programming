cece34bbde041f16abe31de72a1b0715afcdea48
Techvert01 <techsavvy91@outlook.com>
2023-04-04 16:31:59 -0700

    commit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,21 @@
-i
+#include "lists.h"
+
+/**
+ * listint_len -the number of elements in listint_t list.
+ *
+ * @h: head pointer
+ *
+ * Return: the number of elements
+ *
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+	h = h->next;
+		count++;
+	}
+	return (count);
+}
