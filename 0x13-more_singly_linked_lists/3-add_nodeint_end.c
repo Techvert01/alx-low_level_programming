338fc88d88edd60f01b466cd95a8d144d377f943
Techvert01 <techsavvy91@outlook.com>
2023-04-04 17:30:19 -0700

    commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,9 +3,11 @@
 /**
   * add_nodeint_end - add node at end of a listint_t list
   *
-  * @head- head of double pointer
-  * @n- int add the list
-  * Return- NULL if it failed
+  * @head: head of double pointer
+  *
+  * @n: int add the list
+  *
+  * Return: NULL if it failed
   */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
