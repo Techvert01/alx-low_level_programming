01d36bab01da15bd3ff08cbb36545aaaa82e5e87
‘“Temitayo <techsavvy91@outlook.com>
2023-05-04 19:22:16 -0700

    commiting

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
+/**
+ * binary_to_uint - changes binary number to an unassigned int
+ * @b: points to a string of 0 and 1 character
+ *
+ * Return: changed numbers, or 0
+ */
+unassigned int binary_to_uint(const char *b)
 {
-for (i = 0; b[i] != '\0'; i++)
-	{
-		j <<= 1;
-		if (b[i] == '1')
-			j += 1;
-	}
-	return (j);
-int i;
+	int i;
 	unsigned int j;
 
 	j = 0;
@@ -19,4 +19,12 @@ int i;
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }
+
