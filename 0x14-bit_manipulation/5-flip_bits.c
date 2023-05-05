bed8e3ca0e38c666cfb9abe43a79f2d2e22a3728
‘“Temitayo <techsavvy91@outlook.com>
2023-05-04 19:59:04 -0700

    commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - flip to get a  num to another num
+ * @n: the 1st num
+ * @m: the 2nd num
+ * Return: the num of bits you would need to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int differnce, result;
+	unsigned int j, i;
+
+	j = 0;
+	result = 1;
+	differnce = n ^ m;
+	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+	{
+		if (result == (differnce & result))
+			j++;
+		result <<= 1;
+	}
+
+	return (j);
+}
